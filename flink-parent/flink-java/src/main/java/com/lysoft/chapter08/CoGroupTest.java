0e5727cb40a936fb9abf0c5048da8d6a7dd898fb
liangyy <liangyy@github.com>
2023-03-01 10:37:34 +0800

    Update CoGroupTest.javau

---
+++ flink-parent/flink-java/src/main/java/com/lysoft/chapter08/CoGroupTest.java
@@ -0,0 +1,70 @@
+package com.lysoft.chapter08;
+
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.CoGroupFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+import java.time.Duration;
+
+/**
+ * 功能说明：测试CoGroup同组连接
+ * author:liangyy
+ * createtime：2023-02-26 16:50:10
+ */
+public class CoGroupTest {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        SingleOutputStreamOperator<Tuple2<String, Long>> stream1 = env.fromElements(
+                Tuple2.of("a", 1000L),
+                Tuple2.of("b", 1000L),
+                Tuple2.of("a", 2000L),
+                Tuple2.of("b", 2000L),
+                Tuple2.of("c", 2000L)
+        ).assignTimestampsAndWatermarks(WatermarkStrategy.<Tuple2<String, Long>>forBoundedOutOfOrderness(Duration.ZERO)
+                .withTimestampAssigner(new SerializableTimestampAssigner<Tuple2<String, Long>>() {
+                    @Override
+                    public long extractTimestamp(Tuple2<String, Long> element, long recordTimestamp) {
+                        return element.f1;
+                    }
+                })
+        );
+
+        SingleOutputStreamOperator<Tuple2<String, Long>> stream2 = env.fromElements(
+                Tuple2.of("a", 3000L),
+                Tuple2.of("b", 3000L),
+                Tuple2.of("a", 4000L),
+                Tuple2.of("b", 4000L),
+                Tuple2.of("d", 4000L)
+        ).assignTimestampsAndWatermarks(WatermarkStrategy.<Tuple2<String, Long>>forBoundedOutOfOrderness(Duration.ZERO)
+                .withTimestampAssigner(new SerializableTimestampAssigner<Tuple2<String, Long>>() {
+                    @Override
+                    public long extractTimestamp(Tuple2<String, Long> element, long recordTimestamp) {
+                        return element.f1;
+                    }
+                })
+        );
+
+        stream1.coGroup(stream2)
+                .where(data -> data.f0)
+                .equalTo(data -> data.f0)
+                .window(TumblingEventTimeWindows.of(Time.seconds(5)))
+                .apply(new CoGroupFunction<Tuple2<String, Long>, Tuple2<String, Long>, String>() {
+                    @Override
+                    public void coGroup(Iterable<Tuple2<String, Long>> first, Iterable<Tuple2<String, Long>> second, Collector<String> out) throws Exception {
+                        out.collect(first.toString() + " --> " + second.toString());
+                    }
+                }).print();
+
+        env.execute();
+    }
+
+}
