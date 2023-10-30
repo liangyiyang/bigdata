b96d7a1470c825b2e38e00eb16d9a5a86a643baa
liangyy <liangyy@github.com>
2023-10-30 14:48:18 +0800

    Update pom.xml, CommonApiTest.java, and SimpleTableExample.java

--- flink-parent/flink-java/src/main/java/com/lysoft/chapter11/CommonApiTest.java
+++ flink-parent/flink-java/src/main/java/com/lysoft/chapter11/CommonApiTest.java
@@ -1,2 +1,107 @@
-package com.lysoft.chapter11;public class CommonApiTest {
+package com.lysoft.chapter11;
+
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.TableEnvironment;
+
+import static org.apache.flink.table.api.Expressions.$;
+
+/**
+ * 功能说明：Table API和SQL的使用测试
+ * author:liangyy
+ * createtime：2023-08-10 10:07:10
+ */
+public class CommonApiTest {
+
+    public static void main(String[] args) throws Exception {
+/*        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);*/
+
+        // 1. 定义环境配置来创建表
+        // 基于blink版本planner进行流处理
+        EnvironmentSettings settings = EnvironmentSettings.newInstance()
+                .inStreamingMode() // 使用流模式
+                .useBlinkPlanner()
+                .build();
+
+        TableEnvironment tableEnv = TableEnvironment.create(settings);
+
+/*        // 1.1 基于老版本planner进行流处理
+        EnvironmentSettings settings1 = EnvironmentSettings.newInstance()
+                .inStreamingMode() // 使用流模式
+                .useOldPlanner()
+                .build();
+
+        TableEnvironment tableEnv1 = TableEnvironment.create(settings1);*/
+
+/*        // 1.2 基于老版本planner进行批处理
+        EnvironmentSettings settings2 = EnvironmentSettings.newInstance()
+                .inBatchMode()
+                .useOldPlanner()
+                .build();
+
+        TableEnvironment tableEnv2 = TableEnvironment.create(settings2);*/
+
+/*        // 1.3 基于blink版本planner进行批处理
+        EnvironmentSettings settings3 = EnvironmentSettings.newInstance()
+                .inBatchMode()
+                .useBlinkPlanner()
+                .build();
+
+        TableEnvironment tableEnv3 = TableEnvironment.create(settings3);*/
+
+        // 2. 创建表
+        tableEnv.executeSql("CREATE TABLE clickTable (" +
+                " user_name STRING" +
+                ",url STRING" +
+                ",ts BIGINT" +
+                ") WITH(" +
+                " 'connector' = 'filesystem'" +
+                ",'path' = 'input/clicks.csv'" +
+                ",'format' = 'csv'" +
+                ")");
+
+        // 3. 表的查询转换
+        // 3.1 调用Table API
+        Table clickTable = tableEnv.from("clickTable");
+        Table resultTable = clickTable.where($("user_name").isEqual("Bob")).select($("user_name"), $("url"));
+
+        tableEnv.createTemporaryView("resultTable", resultTable);
+
+        // 3.2 执行SQL进行表的查询转换
+        Table resultTable2 = tableEnv.sqlQuery("select url, user_name from resultTable");
+
+        // 3.3 执行聚合计算的查询转换
+        Table aggResult = tableEnv.sqlQuery("select user_name, count(url) cnt from clickTable group by user_name");
+
+        // 4. 创建一张用于输出的表
+        tableEnv.executeSql("CREATE TABLE fileOutputTable (" +
+                " url STRING" +
+                ",user_name STRING" +
+                ") WITH(" +
+                " 'connector' = 'filesystem'" +
+                ",'path' = 'output'" +
+                ",'format' = 'csv'" +
+                ")");
+
+        // 创建一张用于控制台打印输出的表
+        tableEnv.executeSql("CREATE TABLE printOutputTable(" +
+                " user_name STRING" +
+                ",cnt BIGINT" +
+                ") WITH(" +
+                " 'connector' = 'print'" +
+                ")");
+
+        // 5. table方式输出表
+//        resultTable.executeInsert("fileOutputTable");
+//        resultTable2.executeInsert("fileOutputTable");
+//        aggResult.executeInsert("printOutputTable");
+
+        // 6. sql方式输出表
+//        tableEnv.executeSql("insert into fileOutputTable select url, user_name from resultTable");
+        tableEnv.executeSql("insert into printOutputTable select user_name, count(url) cnt from clickTable group by user_name");
+    }
+
 }
--- flink-parent/flink-java/src/main/java/com/lysoft/chapter11/SimpleTableExample.java
+++ flink-parent/flink-java/src/main/java/com/lysoft/chapter11/SimpleTableExample.java
@@ -1,2 +1,66 @@
-package com.lysoft.chapter11;public class SimpleTableExample {
+package com.lysoft.chapter11;
+
+import com.lysoft.chapter05.ClickSource;
+import com.lysoft.chapter05.Event;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+import java.time.Duration;
+
+import static org.apache.flink.table.api.Expressions.$;
+
+/**
+ * 功能说明：Table API和SQL的使用测试
+ * author:liangyy
+ * createtime：2023-08-10 10:07:10
+ */
+public class SimpleTableExample {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        // 1. 读取数据，得到DataStream
+        SingleOutputStreamOperator<Event> eventStream = env.addSource(new ClickSource())
+                .assignTimestampsAndWatermarks(WatermarkStrategy.<Event>forBoundedOutOfOrderness(Duration.ZERO)
+                        .withTimestampAssigner(new SerializableTimestampAssigner<Event>() {
+                            @Override
+                            public long extractTimestamp(Event element, long recordTimestamp) {
+                                return element.getTimestamp();
+                            }
+                        }));
+
+        // 2. 创建表执行环境
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+
+        // 3. 将DataStream转换成Table
+        Table clickTable = tableEnv.fromDataStream(eventStream);
+
+        // 4. 直接写SQL进行查询转换
+        Table resultTable1 = tableEnv.sqlQuery("select url, user, `timestamp` from " + clickTable);
+
+        // 5. 基于Table直接转换
+        Table resultTable2 = clickTable.select(
+                $("url"), $("user"))
+                .where($("user")
+                        .isEqual("Mary")
+                );
+
+        // 6. 转换成流打印输出
+        tableEnv.toDataStream(resultTable1).print("result1");
+        tableEnv.toDataStream(resultTable2).print("result2");
+
+        // 7. 聚合转换
+        tableEnv.createTemporaryView("clickTable", clickTable);
+        Table aggResult = tableEnv.sqlQuery("select user, count(url) cnt from clickTable group by user");
+
+        tableEnv.toChangelogStream(aggResult).print("agg");
+
+        env.execute();
+    }
+
 }
